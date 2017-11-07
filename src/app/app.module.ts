794fd75e2023b0a5bd6e9e429ce4423c94894eab
Neilor Caldeira <neilorcaldeira@gmail.com>
2017-11-07 14:10:59 -0200

    feat(router): add root routes with welcome root page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.routing';
 
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+export const AppRoutes: Routes = [{
+  path: '',
+  component: WelcomeComponent,
+}];
